d4f113f07b4d6c5cf14ba7cc0ea7bfc0d6ba4706
Arnold LAMBOU <lambouarnold@gmail.com>
2022-03-08 21:54:54 +0100

    numberItemMax and numberItemMin unsafe nullable access bug fixed

--- lib/request/validation/rules/numberItemMax.ts
+++ lib/request/validation/rules/numberItemMax.ts
@@ -1,3 +1,4 @@
+import { isFilled } from "../helpers";
 import { Validator } from "../validator";
 
 /**
@@ -8,9 +9,7 @@ import { Validator } from "../validator";
  */
 const numberItemMaxRule = (max: string | number): Validator.RuleType => {
   return {
-    message: (_value, field, _origin, def, data) => {
-      // load type
-      const type = Array.isArray(def.type) ? def.type[0] : def.type;
+    message: (_value, field, _origin) => {
       // load max value
       const maxValue = typeof max === "number" ? max : Number(max);
 
@@ -18,19 +17,23 @@ const numberItemMaxRule = (max: string | number): Validator.RuleType => {
     },
     validator: (value, _field, _origin, def) => {
       try {
-        // load type
-        const type = Array.isArray(def.type) ? def.type[0] : def.type;
-        // load max value
-        const maxValue = typeof max === "number" ? max : Number(max);
+        if (isFilled(value)) {
+          // load type
+          const type = Array.isArray(def.type) ? def.type[0] : def.type;
+          // load max value
+          const maxValue = typeof max === "number" ? max : Number(max);
 
-        switch (type) {
-          case "array":
-            return Array.from(value).every(
-              (item) => typeof item === "number" && item <= maxValue
-            );
+          switch (type) {
+            case "array":
+              return Array.from(value).every(
+                (item) => typeof item === "number" && item <= maxValue
+              );
 
-          default:
-            return true;
+            default:
+              return true;
+          }
+        } else {
+          return true;
         }
       } catch (error) {
         return error.message ?? false;
--- lib/request/validation/rules/numberItemMin.ts
+++ lib/request/validation/rules/numberItemMin.ts
@@ -1,3 +1,4 @@
+import { isFilled } from "../helpers";
 import { Validator } from "../validator";
 
 /**
@@ -8,9 +9,7 @@ import { Validator } from "../validator";
  */
 const numberItemMinRule = (min: string | number): Validator.RuleType => {
   return {
-    message: (_value, field, _origin, def, data) => {
-      // load type
-      const type = Array.isArray(def.type) ? def.type[0] : def.type;
+    message: (_value, field, _origin) => {
       // load min value
       const minValue = typeof min === "number" ? min : Number(min);
 
@@ -18,19 +17,23 @@ const numberItemMinRule = (min: string | number): Validator.RuleType => {
     },
     validator: (value, _field, _origin, def) => {
       try {
-        // load type
-        const type = Array.isArray(def.type) ? def.type[0] : def.type;
-        // load min value
-        const minValue = typeof min === "number" ? min : Number(min);
+        if (isFilled(value)) {
+          // load type
+          const type = Array.isArray(def.type) ? def.type[0] : def.type;
+          // load min value
+          const minValue = typeof min === "number" ? min : Number(min);
 
-        switch (type) {
-          case "array":
-            return Array.from(value).every(
-              (item) => typeof item === "number" && item >= minValue
-            );
+          switch (type) {
+            case "array":
+              return Array.from(value).every(
+                (item) => typeof item === "number" && item >= minValue
+              );
 
-          default:
-            return true;
+            default:
+              return true;
+          }
+        } else {
+          return true;
         }
       } catch (error) {
         return error.message ?? false;
