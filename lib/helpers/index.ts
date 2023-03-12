e532671be5ab7f95f24663b47f5be8c0ba6b84da
Arnold LAMBOU <lambouarnold@gmail.com>
2023-03-12 16:12:24 +0100

    ExpressParser exposed

--- lib/helpers/index.ts
+++ lib/helpers/index.ts
@@ -2,3 +2,4 @@ export { default as Logger } from "./Logger";
 export { default as WorkerHelper } from "../modules/pool/helpers/WorkerHelper";
 export { default as LocalWorkerPoolHelper } from "./LocalWorkerPoolHelper";
 export { default as HttpStatus } from "./HttpStatus";
+export { default as ExpressParser } from "./ExpressParser"
